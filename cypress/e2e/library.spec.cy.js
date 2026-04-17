feb471ce05af469f450532770fafcef19bfd153b
Anthony0486 <anthony.pruniere@wanadoo.fr>
2026-04-17 15:44:36 +0200

    ci : modify cypress library test

--- cypress/e2e/library.spec.cy.js
+++ cypress/e2e/library.spec.cy.js
@@ -1,18 +1,19 @@
-describe('Test du formulaire avec fixture', () => {
+describe('Test du formulaire avec fixture', function () {
       it('Remplir le formulaire avec des utilisateur valide depuis la fixture', function () {
-          cy.fixture('users').as('userData'); 
-          const users = this.userData.users;  
+        cy.visit('http://127.0.0.1:8000'); 
+        cy.fixture('users').as('userData'); 
+        const users = this.userData.users;  
           cy.wrap(users).each((entry) => {
          
             const user = entry.standardUser;
-            cy.visit('http://127.0.0.1:8000');
-           
+             
+            cy.get('href[href="/register"]').click();
             cy.get('#firstname').type(user.firstName);
             cy.get('#lastname').type(user.lastName);
             cy.get('#email').type(user.email);
             cy.get('#password').type(user.password);
             cy.get('#confirm-password').type(user.password);
-            cy.get('href[href="/register"]').click();
+            
             cy.get('button[type="submit"]').click();
 
         });             
