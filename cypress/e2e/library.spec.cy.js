c3323f7dbc9fc4e0480b8e7bbd809d24a252d8f8
Anthony0486 <anthony.pruniere@wanadoo.fr>
2026-04-17 15:35:33 +0200

    ci : modify cypress test

--- cypress/e2e/library.spec.cy.js
+++ cypress/e2e/library.spec.cy.js
@@ -1,20 +1,21 @@
 describe('Test du formulaire avec fixture', () => {
       it('Remplir le formulaire avec des utilisateur valide depuis la fixture', function () {
-        cy.fixture('users').as('userData');
-        const users = this.userData.users;        
-       cy.wrap(users).each((entry) => {
-        cy.visit('http://127.0.0.1:8000');
-        
-        const user = entry.standardUser;
-             cy.get('href[href="/register"]').click();
-             cy.get('#firstname').type(user.firstName);
-             cy.get('#lastname').type(user.lastName);
-             cy.get('#email').type(user.email);
-             cy.get('#password').type(user.password);
-             cy.get('#confirm-password').type(user.password);
-             
-             cy.get('button[type="submit"]').click();
-       });             
+          cy.fixture('users').as('userData'); 
+          const users = this.userData.users;  
+          cy.wrap(users).each((entry) => {
+         
+            const user = entry.standardUser;
+            cy.visit('http://127.0.0.1:8000');
+           
+            cy.get('#firstname').type(user.firstName);
+            cy.get('#lastname').type(user.lastName);
+            cy.get('#email').type(user.email);
+            cy.get('#password').type(user.password);
+            cy.get('#confirm-password').type(user.password);
+            cy.get('href[href="/register"]').click();
+            cy.get('button[type="submit"]').click();
+
+        });             
     });
 });
 
