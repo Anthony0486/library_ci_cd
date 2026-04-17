7a3fdaac50ddb1872d88174441fa8382d5c4fb00
Anthony0486 <anthony.pruniere@wanadoo.fr>
2026-04-17 13:57:16 +0200

    ci: modify url in cypress tests

--- cypress/e2e/library2.spec.cy.js
+++ cypress/e2e/library2.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Test du formulaire avec fixture', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:8000/');
+    cy.visit('http://127.0.0.1:8080/');
     cy.fixture('users').as('userData');
     cy.fixture('books').as('bookData');
   });
